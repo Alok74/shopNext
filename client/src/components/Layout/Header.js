179d6e89d965ff12ceccc3b9ef47120e5aad7059
Alok 74 <alokverma0122@gmail.com>
2024-02-25 22:55:49 +0530

    clear chnages

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -40,7 +40,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              🛒  SHOP Next Hello world
+              🛒  SHOP Next 
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <SearchInput />
